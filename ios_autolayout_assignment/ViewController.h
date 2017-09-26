9a54d7ce3d34b96686912de4263de75423100709
Student 01 <student01@felixs-macbook-pro-2.local>
2017-09-26 13:09:26 +0530

    Initial Commit

---
+++ ios_autolayout_assignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ios_autolayout_assignment
+//
+//  Created by Student 01 on 14/09/17.
+//  Copyright © 2017 mohini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ios_autolayout_assignment/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  ios_autolayout_assignment
+//
+//  Created by Student 01 on 14/09/17.
+//  Copyright © 2017 mohini. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
